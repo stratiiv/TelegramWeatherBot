5ffd3991ecbfcfa957d5ddff57cfc28ed155f451
stratiiv <stratiyvalter@gmail.com>
2023-06-13 20:50:25 +0300

    refactor get weather service

--- services.py
+++ services.py
@@ -6,19 +6,35 @@ API_KEY = "7598a17dae98564b0adfa271f40a66e7"
 API_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 
-def get_current_weather(city: str) -> Tuple[str]:
+def get_current_weather(location: str | Tuple[float, float]) -> Tuple[str]:
     """
     Get weather data from OpenWeatherMap API.
-    Returns a tuple containing the current temperature and 
-    weather description.
+    Args:
+        location (str | Tuple[float, float]):
+        The location can be either a city name (str) or
+        a tuple containing latitude and longitude coordinates (float).
+    Returns:
+        Tuple[str]: A tuple containing the current temperature and
+        weather description.
     """
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric"
-    }
+
+    if isinstance(location, str):
+        params = {
+            "q": location,
+            "appid": API_KEY,
+            "units": "metric"
+        }
+    elif isinstance(location, tuple):
+        params = {
+            "lat": location[0],
+            "lon": location[1],
+            "appid": API_KEY, 
+            "units": "metric"
+        }
+
     r = requests.get(API_URL, params=params)
     data = r.json()
     return data['main']['temp'], data['weather'][0]['description']
 
 
+get_current_weather("ABAHGAH")
